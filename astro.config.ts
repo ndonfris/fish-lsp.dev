373ddb9874aab0efeb178dd6e493f52d7afdd01f
ndonfris <ndonfris29@gmail.com>
2024-04-02 22:41:29 -0500

    feat: added sitemap && robots.txt

---
+++ astro.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
+import robotsConfig from './robots-txt.config'
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://fish-lsp.dev",
+  integrations: [
+    icon(),
+    tailwind({nesting: true}),
+    robotsTxt(robotsConfig),
+    sitemap(),
+  ]
+});
