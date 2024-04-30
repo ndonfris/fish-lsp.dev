9339a9a728afe7ed9d33ab9e44e9bcddafc89619
ndonfris <ndonfris29@gmail.com>
2024-04-30 03:58:41 -0500

    feat: Updated build script to exclude examples in help message
    Why: The build script for generating help messages needed to be updated to exclude the
    "Examples" section from the output. This was done to improve the clarity of the help
    messages and make them more concise.
    
    Changes:
    - Modified the build-output-screenshot.fish script to include a new function __no_examples that filters out the Examples section from the help message.
    - Updated the script to use the new function __no_examples when generating the help message output.
    - Added new files: freeze.png, public/coloricon.svg, public/fixed_video.gif, public/help-msg-new.png, public/new_output.gif.
    - Updated the index.astro to adjust some styling elements in the page layout.

--- build-output-screenshot.fish
+++ build-output-screenshot.fish
@@ -12,9 +12,21 @@ if not command -q fish-lsp
     return 1
 end
 
+
+function __no_examples
+    for a in (fish-lsp --help | string collect | string split \n -n );
+        if test "$a" = "Examples:"
+            return 0
+        end;
+        echo $a
+    end;
+end;
+
 if test -z $argv
-    freeze --config user --execute 'fish-lsp --help' -l bash -t dracula --window --output ./public/help-msg.svg
-    freeze --config user --execute 'fish-lsp --help' -l bash -t dracula --window --output ./public/help-msg.png
+    # freeze --config user --execute 'fish-lsp --help | grep -B99999 "^Examples:" | grep -v "^Examples:"' -l bash -t dracula --window --output ./public/help-msg.svg
+    # freeze --config user --execute 'fish-lsp --help | grep -B99999 "^Examples:" | grep -v "^Examples:"' -l bash -t dracula --window --output ./public/help-msg.png
+    # fish-lsp --help | grep -B99999 "^Examples:" | grep -v "^Examples:"
+    freeze --execute=__no_examples
     and return 
 end
 
